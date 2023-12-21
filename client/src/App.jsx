e88079f289ff80c3e85e16e1584a94fa42660db3
[sharoun <[madoyasharoun@gmail.com]>
2023-12-21 12:37:55 +0300

    :memo: linking the js

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -9,9 +9,26 @@ import './App.css'
 
 function App() {
   return (
-    <>
-    Welcome Shar 
-    </>
+    <Router>  
+      <div className="App">  
+        <ul className="App-header">  
+          <li>  
+            <Link to="/">Home</Link>  
+          </li>  
+          <li>  
+            <Link to="/about">About Us</Link>  
+          </li>  
+          <li> 
+           <Link to="/contact">Contact Us</Link>  
+          </li>  
+        </ul>  
+        <Routes>  
+          <Route exact path='/' element={< Home />}></Route>  
+          <Route exact path='/about' element={< About />}></Route>  
+          <Route exact path='/contact' element={< Contact />}></Route>  
+        </Routes>  
+      </div>
+    </Router>
   )
 }
 
